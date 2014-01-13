f6fab4cb19db0d2fcc58c7e0b150a5f36b414d4e
Jeff Bayes <jbayes92@gmail.com>
2014-01-12 23:06:56 -0800

    Fixed README and #7

--- PE_7.py
+++ PE_7.py
@@ -3,7 +3,7 @@ Project Euler Problem #7 - 10,001st Prime
 By listing the first six prime numbers (2, 3, 5, 7, 11, 13), we can see that the 6th prime is 13.
 What is the 10,001st prime number? """
 
-def prime_finder(primes, number):
+def is_prime(primes, number):
     for element in primes:
         if number % element == 0:
             return primes
@@ -14,7 +14,7 @@ def prime_finder(primes, number):
 primes = [2,3,5,7,11,13]
 number = 2
 while len(primes) != 10001:            
-    primes = prime_finder(primes, number)
+    primes = is_prime(primes, number)
     number += 1
     
 print primes[-1]    
---
+++ README.md
@@ -0,0 +1,10 @@
+Hi, and welcome to my Project Euler solutions!
+
+I just started this early in January of 2014, so I'm not too far along. Between school work and paid work,
+this will probably come along slowly, but I want to practice posting to GitHub, forcing myself to write
+clean code on a regular basis, and challenge my own problem solving skills in order to complete as many
+of the questions posed in Project Euler. Please, take a look at my solutions and if you have any critiques
+on the way I wrote the code, I'm always listening.
+
+Currently completed:
+# 1, 2, 5, 6, 7
